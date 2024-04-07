c9f5973ae3def5954b1949c88e4facd9944a329e
clyvissianos <c.lyvissianos@gmail.com>
2024-04-07 14:03:02 +0300

    Add project files.

---
+++ ConsoleApp.UserInputDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿// Declare variables
+string name = string.Empty; // "";
+int age = 0;
+int retirementAge = 65;
+
+// Prompt the user for input
+Console.WriteLine("Please enter your name:");
+name =Console.ReadLine();
+Console.WriteLine("Please enter your age:");
+age = Convert.ToInt32(Console.ReadLine());
+
+// Process the data
+int workingYearsRemaining = retirementAge - age;
+
+// Output the result to the user
+Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Different datatypes
+/*
+ *  text - string
+ *  integers - int
+ *  decimal - double, float, decimal (different between precision)
+ *  logical - bool
+ * 
+ */
+
+string name = "Kostas";
+
+Console.WriteLine(name);
+Console.WriteLine("I am " + name); // String concatenation
+Console.WriteLine($"They call me {name}"); // String interpolation
+Console.WriteLine("I was given the name {0}", name); // Formated string
+
+int age = 45;
+int retirementYearsLeft = 11;
+int retirementAge = age + retirementYearsLeft;
+
+Console.WriteLine("My age is: " + age);
+Console.WriteLine("My retirement age is: " + retirementAge);
+
+bool isRetired  = false;
+Console.WriteLine("Am I retired? " + isRetired);
