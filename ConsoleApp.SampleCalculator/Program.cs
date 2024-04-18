68b4785117ab7ba99b247740a01f32671452e6ed
clyvissianos <c.lyvissianos@gmail.com>
2024-04-18 19:52:44 +0300

    Added Exception Handling and Modified the Calculator

--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,34 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// Welcome Message
-Console.WriteLine("********* - Welcome to the sample calculator! - ********** \n");
+// Variable Declarations
+int choice = 0;
+int num1, num2 = 0;
 
-int choice;
+// Show calculator options / Show menu
 do
 {
-    // Show calculator options
-    Console.WriteLine("Calculations menu (-1 to exit): ");
-    Console.WriteLine("1 - Addition");
-    Console.WriteLine("2 - Subtration");
-    Console.WriteLine("3 - Multiplication");
-    Console.WriteLine("4 - Division");
-    Console.WriteLine("5 - Modulus");
-    Console.WriteLine("6 - Fibonacci sequence");
-    Console.Write("Please choose calculation: ");
-    choice = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine();
-
-    if (choice >= 1 && choice <= 6)
+    try
     {
-        // Prompt for user input
-        Console.Write("Please enter the first number: ");
-        int num1 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Please enter the second number: ");
-        int num2 = Convert.ToInt32(Console.ReadLine());
+        // Welcome Message
+        Console.Clear();
+        Console.WriteLine("********* - Welcome to the sample calculator! - ********** \n");
         Console.WriteLine();
 
-        // Deide which opration is needed based on selected option
+        // Show calculator options
+        Console.WriteLine("Calculations menu (-1 to exit): ");
+        Console.WriteLine("1 - Addition");
+        Console.WriteLine("2 - Subtraction");
+        Console.WriteLine("3 - Multiplication");
+        Console.WriteLine("4 - Division");
+        Console.WriteLine("5 - Modulus");
+        Console.WriteLine("6 - Fibonacci sequence");
+        Console.Write("Please choose calculation: ");
+
+        choice = Convert.ToInt32(Console.ReadLine());
+ 
+        if (choice == -1)
+        {
+            break;
+        }
+
+        // Prompt for user input
+        Console.WriteLine();
+        Console.Write("Please enter the first number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Please enter the second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+
+        // Decide which operation is needed based on selected option
         int calculationResult = 0;
         switch (choice)
         {
@@ -53,6 +66,8 @@ do
                     calculationResult += i;
                 }
                 break;
+            default:
+                throw new Exception("Invalid Menu Item Selected.");
         }
 
         // print output
@@ -60,20 +75,21 @@ do
         Console.WriteLine("Press Enter to run again");
         Console.ReadLine();
         Console.Clear();
-
     }
-    else if (choice == -1)
+    catch (DivideByZeroException)
     {
-        continue;
+        Console.WriteLine("Cannot divide by zero");
     }
-    else
+    catch (Exception ex)
     {
-        Console.WriteLine("Invalid choice!");
-        Console.WriteLine("Press Enter to run again");
+        Console.WriteLine(ex.Message);
+    }
+    finally
+    {
+        Console.WriteLine("Press any key to continue");
         Console.ReadLine();
-        Console.Clear();
     }
- 
+    
 } while (choice != -1);
 
 Console.WriteLine("********* - Thank you for using the sample calculator! - **********");
