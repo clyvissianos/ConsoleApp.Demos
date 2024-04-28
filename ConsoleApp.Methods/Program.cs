e3c73b04673661d1d144d90381ab51e2db0aa530
clyvissianos <c.lyvissianos@gmail.com>
2024-04-28 12:35:28 +0300

    Added methods and modified the calculator

---
+++ ConsoleApp.Methods/Program.cs
@@ -0,0 +1,84 @@
+﻿Console.WriteLine("************ - Methods - ************");
+
+// void methods - complete a task without returning a value
+void PrintName()
+{
+    // Method code
+    Console.WriteLine("Chester");
+}
+
+// value returning methods - returns a value after an operation
+int GetFiveYearsAgo()
+{
+    int year = DateTime.Now.AddYears(-5).Year;
+    return year;
+}
+
+// methods with parameters - represent data being passed into a method
+void PrintNameWithParam(string name)
+{
+    // Method code
+    Console.WriteLine("Your name is " + name);
+}
+
+int GetYearDifferenceWithParam(int year)
+{
+    int yearDifference = DateTime.Now.Year - year;
+    return yearDifference;
+}
+
+// methods with optional parameters - parameter is not required..also has a default value 
+int GetFutureOrPastYear(int numberOfYears = 0)
+{
+    var year = DateTime.Now.AddYears(numberOfYears).Year;
+    return year;
+}
+
+// methods with nullable parameters
+void PrintNAmeNullableParam(string? name, int? count)
+{
+    //if (string.IsNullOrEmpty(name))
+    //{
+    //    name = "Default Name";
+    //}
+
+    //if(!count.HasValue)
+    //{
+    //    count = 1;
+    //}
+
+    name ??= "Default Name"; // ?? checks if the value on the left is null and then assigns to it the value on the right
+    count ??= 1;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(name);
+    }
+}
+
+
+/* Function Calls */
+PrintName();
+int fiveYearsAgo = GetFiveYearsAgo();
+Console.WriteLine("Five years ago was " + fiveYearsAgo);
+
+Console.Write("Enter your name: ");
+string name1 = Console.ReadLine();
+PrintNameWithParam(name1);
+
+Console.Write("Enter a year: ");
+int pastYear = Convert.ToInt32(Console.ReadLine());
+int yearsAgo = GetYearDifferenceWithParam(pastYear);
+Console.WriteLine("This was " + yearsAgo + " years ago");
+
+Console.Write("Enter number of years in the future or the past: ");
+int numberOfYears = Convert.ToInt32(Console.ReadLine());
+
+var pastYear1 = GetFutureOrPastYear();
+Console.WriteLine("The year is " + pastYear1);
+
+var pastYear2 = GetFutureOrPastYear(numberOfYears);
+Console.WriteLine("The year is " + pastYear2);
+
+PrintNAmeNullableParam(null, null);
+PrintNAmeNullableParam("Chester", 5);
--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -3,26 +3,14 @@
 // Variable Declarations
 int choice = 0;
 int num1, num2 = 0;
+int calculationResult = 0;
 
 // Show calculator options / Show menu
 do
 {
     try
     {
-        // Welcome Message
-        Console.Clear();
-        Console.WriteLine("********* - Welcome to the sample calculator! - ********** \n");
-        Console.WriteLine();
-
-        // Show calculator options
-        Console.WriteLine("Calculations menu (-1 to exit): ");
-        Console.WriteLine("1 - Addition");
-        Console.WriteLine("2 - Subtraction");
-        Console.WriteLine("3 - Multiplication");
-        Console.WriteLine("4 - Division");
-        Console.WriteLine("5 - Modulus");
-        Console.WriteLine("6 - Fibonacci sequence");
-        Console.Write("Please choose calculation: ");
+        PrintMenu();
 
         choice = Convert.ToInt32(Console.ReadLine());
  
@@ -42,29 +30,26 @@ do
 
 
         // Decide which operation is needed based on selected option
-        int calculationResult = 0;
+        
         switch (choice)
         {
             case 1:
-                calculationResult = num1 + num2;
+                calculationResult = AddNumbers(num1, num2);
                 break;
             case 2:
-                calculationResult = num1 - num2;
+                calculationResult = SubtractNumbers(num1, num2);
                 break;
             case 3:
-                calculationResult = num1 * num2;
+                calculationResult = Product(num1, num2);
                 break;
             case 4:
-                calculationResult = num1 / num2;
+                calculationResult = Quotient(num1, num2);
                 break;
             case 5:
-                calculationResult = num1 % num2;
+                calculationResult = Mod(num1, num2);
                 break;
             case 6:
-                for (int i = num1; i <= num2; i++)
-                {
-                    calculationResult += i;
-                }
+                calculationResult = Fibonacci(num1, num2);
                 break;
             default:
                 throw new Exception("Invalid Menu Item Selected.");
@@ -75,6 +60,7 @@ do
         Console.WriteLine("Press Enter to run again");
         Console.ReadLine();
         Console.Clear();
+
     }
     catch (DivideByZeroException)
     {
@@ -89,7 +75,61 @@ do
         Console.WriteLine("Press any key to continue");
         Console.ReadLine();
     }
-    
+
 } while (choice != -1);
 
 Console.WriteLine("********* - Thank you for using the sample calculator! - **********");
+
+// Method Definitions
+int AddNumbers(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int SubtractNumbers(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int Product(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+int Quotient(int num1, int num2)
+{
+    return num1 / num2;
+}
+
+int Mod(int num1, int num2)
+{
+    return num1 % num2;
+}
+
+int Fibonacci(int num1, int num2)
+{
+    var answer = 0;
+    for (int i = num1; i <= num2; i++)
+    {
+        answer += i;
+    }
+    return answer;
+}
+
+void PrintMenu()
+{
+    // Welcome Message
+    Console.Clear();
+    Console.WriteLine("********* - Welcome to the sample calculator! - ********** \n");
+    Console.WriteLine();
+
+    // Show calculator options
+    Console.WriteLine("Calculations menu (-1 to exit): ");
+    Console.WriteLine("1 - Addition");
+    Console.WriteLine("2 - Subtraction");
+    Console.WriteLine("3 - Multiplication");
+    Console.WriteLine("4 - Division");
+    Console.WriteLine("5 - Modulus");
+    Console.WriteLine("6 - Fibonacci sequence");
+    Console.Write("Please choose calculation: ");
+}
